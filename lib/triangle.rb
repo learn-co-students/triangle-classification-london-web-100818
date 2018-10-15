c6a609ac973a8710b8f59f3245b11791baf40a36
natashabuck <natashabuck1@gmail.com>
2018-10-15 09:29:42 +0100

    completed traingle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+
+  attr_accessor :side1, :side2, :side3, :kind
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if @side1 <= 0 or @side2 <= 0 or @side3 <= 0
+        raise TriangleError
+    else
+      if (@side1 + @side2 <= @side3) or (@side2 + @side3 <= @side1) or (@side3 + @side1 <= @side2)
+        raise TriangleError
+      else
+        if @side1 == @side2 && @side2 == @side3
+          @kind = :equilateral
+        elsif @side1 == @side2 or @side2 == @side3 or @side1 == @side3
+          @kind = :isosceles
+        else
+          @kind = :scalene
+        end
+      end
+    end
+    @kind
+  end
+  class TriangleError < StandardError
+  end
 end
