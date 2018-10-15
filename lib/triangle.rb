5b0257ce9893a205e562bfb3902c6b6d273c2946
github username <github email address>
2018-10-15 11:31:12 +0100

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
+require'pry'
 class Triangle
-  # write code here
+  attr_accessor :first, :second, :third
+def initialize(first,second,third)
+@first=first
+@second=second
+@third=third
+@array=[@first,@second,@third]
+end
+def kind
+
+  if (@array.any? {|x| x<=0} || @array.any? {|x| @array.inject(:+)-x<=x} )
+
+    raise TriangleError
+
+elsif @array.uniq.length==1
+  :equilateral
+elsif @array.uniq.length==2
+  :isosceles
+elsif @array.uniq.length==3
+  :scalene
+end
+end
+
+
+  class TriangleError < StandardError
+
+  end
 end
