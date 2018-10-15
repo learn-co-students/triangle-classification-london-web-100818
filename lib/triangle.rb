fbbbd1be01f4fb98bedc50401a6b3b2cb724097b
github username <slonanuga@gmail.com>
2018-10-15 16:24:17 +0100

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+
+  attr_accessor :l1, :l2, :l3
+  
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+
+  end
+
+  def kind
+    if @l1 + @l2 <= @l3 || @l2 + @l3 <= @l1 || @l1 + @l3 <= @l2 || @l1 <= 0 || @l2 <= 0 || @l3 <= 0
+      begin
+        raise TriangleError
+      end
+    end
+
+    case [@l1,@l2,@l3].uniq.length
+    when  1
+      :equilateral
+    when 2
+      :isosceles
+    when 3
+      :scalene
+    end
+  end
+  
+
+
+
+  class TriangleError < StandardError
+    def message
+      "The sum of two triangle lengths must exceed the value! Please enter valid lengths."
+    end
+  end
 end
